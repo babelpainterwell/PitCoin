3e13ea5dbc94564a8e0d9dcd40de29f9036c2bf8
babelpainterwell <zhang.zhongw@northeastern.edu>
2025-02-20 20:44:06 -0500

    add ComputeMerkleRoot to block

--- internal/block/block.go
+++ internal/block/block.go
@@ -1,6 +1,8 @@
 package block
 
 import (
+	"bytes"
+
 	"github.com/babelpainterwell/shitcoin/internal/hashutil"
 	"github.com/babelpainterwell/shitcoin/internal/transaction"
 )
@@ -19,20 +21,79 @@ type BlockHeader struct {
 	Nonce         uint32 
 }
 
+func (bh *BlockHeader) SerializeHeader() []byte {
+	// serialize the block header
+
+	var buf bytes.Buffer
+
+	// serialization of all fields
+	//
+	//
+	//
+	//
+
+	return buf.Bytes()
+}
+
+func (bh *BlockHeader) BlockHash() [32]byte {
+	headerBytes := bh.SerializeHeader()
+	return hashutil.DoubleSha256(headerBytes)
+}
+
 type Block struct {
 	Header 	 BlockHeader
 	Transactions []*transaction.Transaction
 }
 
+
+// ComputeMerkleRoot calculates the merkle root by hashing all TxIDs in the block pairwise 
+// If there is an odd number of transactions, re repeat the last one.
+// As DoubleSha256 requires a byte slice, we need to convert the [32]byte to a []byte before hashing. 
 func (b *Block) ComputeMerkleRoot() [32]byte {
-	//
+
+	// 1. Get all the transaction IDs
+	txIDs := make([][32]byte, 0, len(b.Transactions))
+	for _, tx := range b.Transactions {
+		txIDs = append(txIDs, tx.GetTxID())
+	} 
+
+	if len(txIDs) == 0 {
+		return [32]byte{}
+	}
+
+	// 2. Convert the transaction IDs to a slice of byte slices
+	level := make([][]byte, 0, len(txIDs))
+	for _, txID := range txIDs {
+		sliceOfTxID := txID[:]
+		level = append(level, sliceOfTxID)
+	}
+
+	// 3. Build the tree until we are only left with one root 
+	for len(level) > 1 {
+		var newLevel [][]byte 
+		for i := 0; i < len(level); i += 2 {
+			// if we have an odd number transactions in current level, we repeat the last one 
+			if i + 1 == len(level) {
+				newLevel = append(newLevel, doubleSha256Concat(level[i], level[i]))
+			} else {
+				newLevel = append(newLevel, doubleSha256Concat(level[i], level[i + 1]))
+			}
+		}
+		level = newLevel
+	}
+
+	var root [32]byte 
+	copy(root[:], level[0])
+
+	return root
 }
 
 
 
 
 // to compute a Merkle node, two 32-byte hashes are concatenated and hashed together.
-func doubleSha256Concat(first, second [32]byte) [32]byte {
-	concat := append(first[:], second[:]...)
-	return hashutil.DoubleSha256(concat)
+func doubleSha256Concat(first, second []byte) []byte {
+	concat := append(first, second...)
+	result := hashutil.DoubleSha256(concat)
+	return result[:]
 }
--- internal/transaction/transaction.go
+++ internal/transaction/transaction.go
@@ -1,5 +1,11 @@
 package transaction
 
+import (
+	"bytes"
+
+	"github.com/babelpainterwell/shitcoin/internal/hashutil"
+)
+
 type TxIn struct {
 }
 
@@ -13,5 +19,20 @@ type Transaction struct {
 	LockTime uint32
 }
 
+func (tx *Transaction) SerializeTransaction() []byte {
+	// serialize the transaction
+	var buf bytes.Buffer
+
+	// serialization of all fields
+	//
+	//
+	//
+
+	return buf.Bytes()
+}
 
 
+func (tx *Transaction) GetTxID() [32]byte {
+	transactionBytes := tx.SerializeTransaction()
+	return hashutil.DoubleSha256(transactionBytes)
+}
