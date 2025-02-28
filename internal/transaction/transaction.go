287a1066cbd15a8872b429815a8b7b41faa48569
babelpainterwell <zhang.zhongw@northeastern.edu>
2025-02-28 01:05:43 -0500

    update Transaction

--- internal/transaction/transaction.go
+++ internal/transaction/transaction.go
@@ -9,15 +9,16 @@ import (
 // In terms of tracking orevious outputs, Bitcoin Core keeps a database that stores every UTXO and essential metadata about it.
 
 type TxInput struct {
-	OutpointTxID [32]byte // transaction ID where the funding should be spent
-	OutputIndex uint32 // index of the output in the transaction
-	InputScript []byte // ???
-	Sequence uint32 // 
+	OutpointTxID 	[32]byte // transaction ID where the funding should be spent
+	OutputIndex 	uint32 // index of the output in the transaction
+	InputScript 	[]byte // ??? scriptSig (for legacy or P2SH spends; empty for native segwit)
+	Sequence 		uint32 // sequence number
+	Witness   		[][]byte // witeness stack
 }
 
 type TxOutput struct {
-	Amount uint64 // amount of shitcoins
-	OutputScript []byte // ???
+	Amount 			uint64 // amount of shitcoins
+	OutputScript 	[]byte // ???scriptPubkey
 }
 
 type Transaction struct {
@@ -28,11 +29,25 @@ type Transaction struct {
 	Version uint32
 	Marker  uint8
 	Flag    uint8
-	TxInCount uint32
-	TxIn []*TxInput
-	TxOutCount uint32
-	TxOut []*TxOutput
+	
+	TxIns 	[]*TxInput
+	TxOuts 	[]*TxOutput
+
+	LockTime uint32 // lock time field
 }
+
+
+// returns true if any input contains witness data
+func (tx *Transaction) isSegwit() bool {
+	for _, in := range tx.TxIns {
+		if len(in.Witness) > 0 {
+			return true
+		}
+	}
+	return false
+}
+
+
 
 func (tx *Transaction) SerializeTransaction() []byte {
 	// serialize the transaction
